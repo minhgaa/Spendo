3415d26dadfadd24abb5877204a3afe83d319ff3
Minhgaa <bminh.cv@gmail.com>
2024-12-30 13:19:49 +0700

    Initial Commit

---
+++ Spendo1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spendo1
+//
+//  Created by minhgaa on 30/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
